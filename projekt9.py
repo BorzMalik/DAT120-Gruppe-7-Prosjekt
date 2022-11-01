de1491d3a21e38bdafa9b758038d112571b94a7b
BorzMalik <116881243+BorzMalik@users.noreply.github.com>
2022-11-01 14:09:15 +0100

    Update projekt9.py

--- projekt9.py
+++ projekt9.py
@@ -33,3 +33,37 @@ class avtale:
   
 avtale_1 = avtale('Markus', 'Madla',x.strftime("%B") ,20)
 avtale_2 = avtale('Nora', 'Storhaug',x.strftime("%B") ,30)
+
+
+oppgave l)
+
+def menu():
+    print("[1] Lese inn avtale.")
+    print("[2] Skrive inn avtale til fil.")
+    print("[3] Skrive inn ny avtale.")
+    print("[4] Skrive ut alle avtaler.")
+    print("[0] Avslutte programmet.")
+    
+    print("Du må ta et valgene til venstre for å velge et alternativ")
+
+menu()
+option = int(input("Skriv inn ditt alternativ: "))
+
+while option != 0:
+    if option == 1:
+        #Do option 1 stuff (etc)
+        print("Du har valgt alternativ 1 - (Lese inn avtale)")
+    elif option == 2:
+        print("Du har valgt alternativ 2 - (Skrive inn avtale til fil)")
+    elif option == 3:
+        print("Du har valgt alternativ 3 - (Skrive inn ny avtale)")
+    elif option == 4:
+        print("Du har valgt alternativ 4 - (Skrive ut alle avtaler)")
+    else:
+        print("Du valgte et utilgjengelig alternativ.")
+
+
+
+    print()
+    menu()
+    option = int(input("Skriv inn ditt alternativ: "))
